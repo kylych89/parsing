8ca2c77267e40265eb32328e4ceeff778c3eadae
kylych <kylycsydykov@gmail.com>
2023-01-26 13:42:19 +0600

    initial commit

---
+++ parse.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import service
+
+# URL_TEMPLATE = "https://www.work.ua/ru/jobs-odesa/?page=2"
+# r = requests.get(URL_TEMPLATE)
+#
+# soup = bs(r.text, "html.parser")
+# ht = int(soup.find('span', class_='text-default').text.split()[-1])
+#
+# tx = ''
+# for page in range(1, 5):
+#     r = requests.get(f'https://www.work.ua/ru/jobs-odesa/?page={page}')
+#     soup = bs(r.text, "html.parser")
+#     vacancies_names = soup.find_all('h2')
+#
+#     for name in vacancies_names:
+#         tx += name.text
+#
+# service.save_data(tx)
+
+
+TEMPLATE_URL = 'https://www.tazabek.kg/news:1855143/?from=portal&place=nowread&b=1'
+
+req = requests.get(TEMPLATE_URL)
+
+soup = bs(req.text, 'html.parser')
+title1 = soup.find('h2')
+
+body1 = soup.find('div', class_='text')
+
+all_text = f'Тема: {title1.text}\n{body1.text}'
+
+service.save_data(all_text)
---
+++ service.py
@@ -0,0 +1,5 @@
+
+
+def save_data(data):
+    with open('data.txt', 'w') as file:
+        file.write(data)
